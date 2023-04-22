1dc294a186b0dac9e2e52f74f5c74b999643307f
JoseOzuna23 <hose-23@hotmail.com>
2023-04-21 20:07:55 -0400

    modificacion de algunos puntos

---
+++ primer_read/src/App.jsx
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App m-5">
+
+      <h1> Hola Dojo </h1>
+      <h2> Lista de Lenguajes</h2>
+      <div className=''>
+
+      
+      <li> PHP</li>
+      <li> html</li>
+      <li> css</li>
+      <li> javascript</li>
+      </div>
+      
+
+
+    </div>
+  );
+}
+
+export default App;
